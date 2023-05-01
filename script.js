44b8b1572d28bda178138c5f38c03454e0ab9ab3
Gruwpy <75033204+Gruwpy@users.noreply.github.com>
2023-05-01 02:19:56 -0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const window1 = document.querySelector('#window1');
-const window2 = document.querySelector('#window2');
-const window3 = document.querySelector('#window3');
-
-// Add content to window1
